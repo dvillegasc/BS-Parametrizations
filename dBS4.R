b41b38cc5b850160b36d8ba2c9bc0ffcabae7918
dvillegasc <david.villegas1@udea.edu.co>
2025-10-21 19:38:11 -0500

    Changes in BS4

--- dBS4.R
+++ dBS4.R
@@ -1,124 +1,71 @@
-#' The Birnbaum-Saunders distribution
-#' 
-#' @description
-#' Density, distribution function, quantile function, 
-#' random generation and hazard function for the 
-#' Birnbaum-Saunders distribution with
-#' parameters \code{mu} and \code{sigma}.
-#' 
-#' @param x,q	vector of quantiles.
-#' @param p vector of probabilities.
-#' @param n number of observations. 
-#' @param mu parameter.    
-#' @param sigma parameter.
-#' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
-#' @param lower.tail logical; if TRUE (default), probabilities are 
-#' P[X <= x], otherwise, P[X > x].
-#' 
-#' @references
-#' Birnbaum, Z.W. and Saunders, S.C. (1969a). A new family of life 
-#' distributions. J. Appl. Prob., 6, 319-327.
-#' 
-#' Roquim, F. V., Ramires, T. G., Nakamura, L. R., Righetto, A. J., 
-#' Lima, R. R., & Gomes, R. A. (2021). Building flexible regression 
-#' models: including the Birnbaum-Saunders distribution in the 
-#' gamlss package. Semina: Ciências Exatas e Tecnológicas, 
-#' 42(2), 163-168.
-#' 
-#' @seealso \link{BS}.
-#' 
-#' @details 
-#' The Birnbaum-Saunders with parameters \code{mu} and \code{sigma}
-#' has density given by
-#' 
-#' \eqn{f(x) = \frac{x^{-3/2}(x+\mu)}{2\sigma\sqrt{2\pi\mu}} \exp\left(\frac{-1}{2\sigma^2}(\frac{x}{\mu}+\frac{\mu}{x}-2)\right)}
-#' 
-#' for \eqn{x>0}, \eqn{\mu>0} and \eqn{\sigma>0}. In this 
-#' parameterization \eqn{\mu} is the median of \eqn{X}, 
-#' \eqn{E(X)=\mu(1+\sigma^2/2)} and 
-#' \eqn{Var(X)=(\mu\sigma)^2(1+5\sigma^2/4)}. The functions 
-#' proposed here
-#' corresponds to the functions created by Roquim et al. (2021)
-#' with minor modifications to obtain correct log-likelihoods
-#' and random samples.
-#' 
-#' @return 
-#' \code{dBS} gives the density, \code{pBS} gives the distribution 
-#' function, \code{qBS} gives the quantile function, \code{rBS}
-#' generates random deviates and \code{hBS} gives the hazard function.
-#' 
-#' @example examples/examples_dBS.R
-#' 
-#' @export
-dBS <- function(x, mu=1, sigma=1, log=FALSE){
+dBS4 <- function(x, mu=1, sigma=0.5, log=FALSE){
   if (any(mu <= 0)) stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma <= 0)) stop(paste("sigma must be positive", "\n", ""))
+  if (any(sigma <= 0))  stop(paste("sigma must be positive", "\n", ""))
   
-  res <- ifelse(x<=0, -9999999,
-                -1.5*log(x)+log(x+mu)-log(2*sigma)-0.5*log(2*pi*mu)-0.5*(x/mu+mu/x-2)/sigma^2)
+  # Changing from BS4 to BS (original)
+  new_mu    <- (sigma/mu)
+  new_sigma <- 1 / sqrt(mu*sigma)
   
-  if (log == TRUE)
-    result <- res
-  else
-    result <- exp(res)
-  return(result)
+  res <- dBS(x=x, mu=new_mu, sigma=new_sigma, log=log)
+  return(res)
 }
-#' @export
-#' @importFrom stats pnorm
-#' @rdname dBS
-pBS <- function(q, mu=1, sigma=1, lower.tail=TRUE, log.p=FALSE){
-  
+
+
+pBS4 <- function(q, mu=1, sigma=0.5, lower.tail=TRUE, log.p=FALSE){
   if (any(mu <= 0))    stop("parameter mu has to be positive!")
-  if (any(sigma <= 0)) stop("parameter sigma has to be positive!")
+  if (any(sigma <= 0))  stop(paste("sigma must be positive", "\n", ""))
   
-  cdf <- pnorm(((q/mu)^0.5-(mu/q)^0.5)/sigma)
+  # Changing from BS4 to BS (original)
+  new_mu    <- (sigma/mu)
+  new_sigma <- 1 / sqrt(mu*sigma)
   
-  if (lower.tail == TRUE) 
-    cdf <- cdf
-  else 
-    cdf = 1 - cdf
-  if (log.p == FALSE) 
-    cdf <- cdf
-  else 
-    cdf <- log(cdf)
-  cdf <- ifelse(q < 0, 0, cdf)
+  cdf <- pBS(q=q, mu=new_mu, sigma=new_sigma, lower.tail=lower.tail, log.p=log.p)
+  
   return(cdf)
 }
-#' @importFrom stats uniroot qnorm
-#' @export
-#' @rdname dBS
-qBS <- function(p, mu=1, sigma=1, lower.tail = TRUE, log.p = FALSE){
+
+qBS4 <- function(p, mu=1, sigma=0.5, lower.tail = TRUE, log.p = FALSE){
   if (any(mu <= 0)) stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma <= 0)) stop(paste("sigma must be positive", "\n", ""))
+  if (any(sigma <= 0 | sigma>=1)) 
+    stop(paste("sigma must be positive", "\n", ""))
+  
+  # Changing from BS4 to BS (original)
+  new_mu    <- (sigma/mu)
+  new_sigma <- 1 / sqrt(mu*sigma)
+  
   if (log.p==TRUE) p <- log(p)
   if (lower.tail==FALSE) p <- 1-p
   if (any(p < 0)|any(p > 1)) stop(paste("p must be between 0 and 1", "\n", ""))
-  w <- sigma * qnorm(p)/2
-  q <- mu * (w + sqrt(w^2+1))^2
+  
+  q <- qBS(p=p, mu=new_mu, sigma=new_sigma, lower.tail=lower.tail, log.p=log.p)
   return(q)
 }
-#' @importFrom stats runif
-#' @export
-#' @rdname dBS
-rBS <- function(n, mu=1, sigma=1){
-  if (any(mu <= 0)) stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma <= 0)) stop(paste("sigma must be positive", "\n", ""))
+
+
+rBS4 <- function(n, mu=1, sigma=0.5){
   if (any(n <= 0)) stop(paste("n must be a positive integer", "\n", ""))
-  n <- ceiling(n)
-  p <- runif(n)
-  r <- qBS(p, mu=mu, sigma=sigma)
-  r
+  if (any(mu <= 0)) stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0))
+    stop(paste("sigma must be positive", "\n", ""))
+      
+      # Changing from BS4 to BS (original)
+      new_mu    <- (sigma/mu)
+      new_sigma <- 1 / sqrt(mu*sigma)
+      
+      r <- rBS(n=n, mu=new_mu, sigma=new_sigma)
+      r
 }
-#' @export
-#' @rdname dBS
-hBS <- function(x, mu, sigma){
+
+
+hBS4 <- function(x, mu, sigma){
   if (any(x < 0)) 
     stop(paste("x must be positive", "\n", ""))
   if (any(mu <= 0 )) 
     stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma <= 0)) 
+  if (any(sigma <= 0))
     stop(paste("sigma must be positive", "\n", ""))
-  
-  h <- dBS(x, mu, sigma) / pBS(x, mu, sigma, lower.tail=FALSE)
-  h
+      
+      h <- dBS4(x, mu, sigma) / pBS4(x, mu, sigma, lower.tail=FALSE)
+      h
 }
+
